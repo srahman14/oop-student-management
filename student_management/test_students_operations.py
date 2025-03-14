1b446ee8aa3fdceab2e3213fe9c7640f47175eb8
srahman14 <s.rahman14@hotmail.com>
2025-03-14 03:31:28 +0000

    testing CRUD functions

---
+++ student_management/test_students_operations.py
@@ -0,0 +1,25 @@
+from student_operations import add_student, list_all_students, update_student, del_student
+
+# add_student(101, "Alice", 19, ["Math", "Physics"])
+# add_student(104, "Bob", 21, ["CS", "AI"])
+# add_student(102, "Charlie", 22, ["Biology"])
+add_student(105, "Yunus", 24, ["Maths", "Chemistry"])
+add_student(107, "Muhammad", 19, ["Maths", "Physics", "Biology"])
+
+
+
+# print("\nListing Students:")
+# list_all_students()
+# print()
+
+# update_student(101, "Alissa", 20, None)
+
+# print()
+# print("\nListing Students:")
+# list_all_students()
+
+# del_student(101)
+# del_student(104)
+# del_student(102)
+
+# list_all_students()
