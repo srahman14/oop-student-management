88bdd1fdc5773dee0116f13956d59df8ce808c55
srahman14 <s.rahman14@hotmail.com>
2025-03-15 03:28:06 +0000

    add course method added, __str__ and __repr__ methods added

--- student_management/models/student.py
+++ student_management/models/student.py
@@ -23,6 +23,7 @@ class Student:
     def get_student_id(self):
         if self.__student_id is None or self.__student_id == 0:
             print("Student ID is not set. Please set ID first.")
+            print("Use: set_student_id('STUDENT_ID_HERE') to set an ID")
             return
 
         return self.__student_id
@@ -30,46 +31,29 @@ class Student:
     def get_student_details(self):
         return f"Student ID: {self.__student_id}\nStudent Name: {self.__name}\nStudent Age: {self.__age}\nStudent Course(s): {self.__courses}"
     
-    def update_course(self):
-        changeCourse = input("Enter the course to change: ").strip().capitalize()
-
+    def update_courses(self, old_course, new_course):
         normalised_courses = [course.capitalize() for course in self.__courses]
 
-        while True:
-            changeCourse = input("Enter the course to change: ").strip().capitalize()
-
-            if changeCourse in normalised_courses:
-                break
-            print(f"\nCourse '{changeCourse}' does not exist")
-            print(f"Current Enrollment:\nStudent Course(s): {self.__courses}\n")
-
-        while True:
-            newCourse = input("Enter the new course: ").capitalize()
-
-            if not newCourse:
-                print("Error: Course name cannot be empty")
-                continue
-            
-            if newCourse in normalised_courses:
-                print(f"Error: Course {newCourse} already exists")
-                continue
-            
-            if newCourse not in normalised_courses:
-                break
+        if old_course.capitalize() not in normalised_courses:
+            print(f"\nCourse '{old_course}' does not exist")
+            print(f"\Student '{self.__name}' Current Enrollment:\nStudent Course(s): {self.__courses}\n")
+            return
+        
+        if new_course.capitalize() in normalised_courses:
+            print(f"\nCourse '{new_course}' already exists")
+            return
+        
         try:
-            if changeCourse.strip() in self.__courses:
-                index = normalised_courses.index(changeCourse)
-                self.__courses[index] = newCourse
-
-                print("Updating courses...")
-                time.sleep(3)
-
-                print(f"\nUpdated Courses: [{', '.join(self.__courses)}]")
+            index = normalised_courses.index(old_course.capitalize().strip())
+            self.__courses[index] = new_course.capitalize().strip()
+            print("Updating courses...")
+            time.sleep(2)
+            print(f"\nUpdated Courses: [{', '.join(self.__courses)}]")
         except ValueError as e:
             print(f"Error updating course: {e}")
     
     def update_name(self, newName: str):
-        print("Update Name:\n- Name must be 2-3 characters minimum\n- Do not include any special characters\n- Do not include any numbers\n") 
+        # print("Update Name:\n- Name must be 2-3 characters minimum\n- Do not include any special characters\n- Do not include any numbers\n") 
         numbers = [str(number) for number in range(0,10)]
 
         # Use set to wrap string in three quotes to handle single/double quotes
@@ -94,14 +78,27 @@ class Student:
             
     
     def update_age(self, age: int):
-        print("Update Age:\n- Age must be 1 character minimum\n") 
+        if len(str(age)) == 0: 
+            print("Update Age:\n- Age must be 1 character minimum\n") 
     
         if type(age) != int:
             print("Only numbers accepted for updating age")
             return
 
         self.__age = age
-    
+
+    def add_courses(self, new_courses):
+        new_courses = [course.capitalize() for course in new_courses]
+
+        for new_course in new_courses:
+            if new_course in self.__courses:
+                print(f"Error: Course '{new_course}' already exists")
+            else:
+                self.__courses.append(new_course)
+                print(f"Added: Course '{new_course}' added for {self.__name}")
+        
+        print(f"Updated Courses \n {', '.join(self.__courses)}")
+
     def get_student_id(self):
         return self.__student_id
 
@@ -114,6 +111,20 @@ class Student:
     def get_courses(self):
         return self.__courses
 
+    # Useful for when object is printed
+    def __str__(self):
+        courses_str = "\n - ".join(self.get_courses())
+        return (f"Student ID: {self.get_student_id()}\n"
+                f"Student Name: {self.get_name()}\n"
+                f"Student Age: {self.get_age()}\n"
+                f"Student Courses:\n - {courses_str}\n"
+                f"---------------------------------------------------------------------------")
+
+    # Useful for debugging
+    def __repr__(self):
+        return f"Student(student_id={self.get_student_id()}, name={self.get_name()}, age={self.get_age()}, courses={self.get_courses()})"
+
+
 class StudentEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, Student):
@@ -121,7 +132,16 @@ class StudentEncoder(json.JSONEncoder):
         
         return super().default(o)
 
+class StudentDecoder(json.JSONDecoder):
+    def __init__(self):
+        super().__init__(object_hook=self.object_hook)
+
+    def object_hook(self, dct):
+        if "student_id" in dct and "name" in dct and "age" in dct and "courses" in dct:
+            return Student(dct["student_id"], dct["name"], dct["age"], dct["courses"])
+        return dct
         
+
 
 # john = Student(0(), "John", 18, ["History", "Spanish", "Maths"])
 # john.get_student_details()
