77146ae32353541665d87ba7dcb9dd37c5b1690d
srahman14 <s.rahman14@hotmail.com>
2025-03-15 03:29:18 +0000

    tests added

--- student_management/test_students_operations.py
+++ student_management/test_students_operations.py
@@ -1,10 +1,10 @@
-from student_operations import add_student, list_all_students, update_student, del_student
+from student_operations import add_student, list_all_students, update_student, add_new_courses, del_student
 
 # add_student(101, "Alice", 19, ["Math", "Physics"])
 # add_student(104, "Bob", 21, ["CS", "AI"])
 # add_student(102, "Charlie", 22, ["Biology"])
-add_student(105, "Yunus", 24, ["Maths", "Chemistry"])
-add_student(107, "Muhammad", 19, ["Maths", "Physics", "Biology"])
+# add_student(105, "Yunus", 24, ["Maths", "Chemistry"])
+# add_student(107, "Muhammad", 19, ["Maths", "Physics", "Biology"])
 
 
 
@@ -12,7 +12,13 @@ add_student(107, "Muhammad", 19, ["Maths", "Physics", "Biology"])
 # list_all_students()
 # print()
 
-# update_student(101, "Alissa", 20, None)
+# update_student(101, "Alice", 19, None)
+# update_student(105, "Yunus", 21, "Maths", "CS")
+# add_new_courses(102, ["Robotics", "Aerospace", "Linguistics"])
+
+
+# add_student(111, "Delete ME", 0, ["DELETE", "ME"])
+del_student(111)
 
 # print()
 # print("\nListing Students:")
