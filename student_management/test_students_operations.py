da8ecc1d4eeb3ae198217d331b0e00a52303fe13
srahman14 <s.rahman14@hotmail.com>
2025-03-19 02:09:54 +0000

    test cases

--- student_management/test_students_operations.py
+++ student_management/test_students_operations.py
@@ -1,24 +1,29 @@
-from student_operations import add_student, list_all_students, update_student, add_new_courses, del_student
+from student_operations import add_student, list_all_students, update_student, add_new_courses, del_student, remove_course
 
+# Adding students
 # add_student(101, "Alice", 19, ["Math", "Physics"])
 # add_student(104, "Bob", 21, ["CS", "AI"])
 # add_student(102, "Charlie", 22, ["Biology"])
 # add_student(105, "Yunus", 24, ["Maths", "Chemistry"])
 # add_student(107, "Muhammad", 19, ["Maths", "Physics", "Biology"])
-
-
+# add_student(190325, "Yusuf", 20, ["Psychology"])
 
 # print("\nListing Students:")
 # list_all_students()
 # print()
 
+
+# Updating students
+
 # update_student(101, "Alice", 19, None)
 # update_student(105, "Yunus", 21, "Maths", "CS")
-# add_new_courses(102, ["Robotics", "Aerospace", "Linguistics"])
+# add_new_courses(190325, ["Robotics", "Aerospace", "Linguistics"])
+# update_student(190325, "Adam", 20, "Psychology", "Sociology")
+# remove_course(190325, "Linguistics")
+# Deleting Students
 
-
-# add_student(111, "Delete ME", 0, ["DELETE", "ME"])
-del_student(111)
+# add_student(111111, "Delete ME", 0, ["DELETE", "ME"])
+# del_student(111111)
 
 # print()
 # print("\nListing Students:")
