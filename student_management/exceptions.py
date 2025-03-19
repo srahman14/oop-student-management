df20dfd9602572b089d429882a6102aedb902139
srahman14 <s.rahman14@hotmail.com>
2025-03-19 02:08:27 +0000

    Exception Handling Added

---
+++ student_management/exceptions.py
@@ -0,0 +1,11 @@
+class InvalidIDException(Exception):
+    # Exceptions for invalid ID input 
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class DuplicateIDExcption(Exception):
+    # Exception for when a dupliate ID is found
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
