4abafdc52ae608411339af8aec6cbb9f0907ff70
Sayed Rahman <s.rahman14@hotmail.com>
2025-03-26 00:02:29 +0000

    misc

--- student_management/test_students_operations.py
+++ student_management/test_students_operations.py
@@ -1,4 +1,4 @@
-from student_operations import add_student, list_all_students, update_student, add_new_courses, del_student, remove_course
+from student_operations import add_student, list_all_students, update_student, add_new_courses, del_student, remove_course, add_new_minors, remove_minor
 
 # Adding students
 # add_student(101, "Alice", 19, ["Math", "Physics"])
@@ -7,7 +7,7 @@ from student_operations import add_student, list_all_students, update_student, a
 # add_student(105, "Yunus", 24, ["Maths", "Chemistry"])
 # add_student(107, "Muhammad", 19, ["Maths", "Physics", "Biology"])
 # add_student(190325, "Yusuf", 20, ["Psychology"])
-add_student(111111, "Saeed", 26, ["Computer Science", "Networking"], True, "Mandarin")
+# add_student(111111, "Saeed", 26, ["Computer Science", "Networking"], True, "Mandarin")
 
 
 # print("\nListing Students:")
@@ -24,8 +24,10 @@ add_student(111111, "Saeed", 26, ["Computer Science", "Networking"], True, "Mand
 # remove_course(190325, "Linguistics")
 # Deleting Students
 
-# add_student(111111, "Delete ME", 0, ["DELETE", "ME"])
-# del_student(111111)
+# add_student(123321, "Delete ME", 0, ["DELETE", "ME"], True, "spanish")
+# del_student(123321)
+# add_new_minors(123321, ["french", "chinese"])
+# remove_minor(123321, "French")
 
 # print()
 # print("\nListing Students:")
